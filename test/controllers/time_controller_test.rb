7900b661caad42b6013160c8374d4984ba5c1bc0
B. Ruschill <brandon.ruschill@gmail.com>
2019-08-07 17:01:12 -0500

    write some basic tests

--- test/controllers/time_controller_test.rb
+++ test/controllers/time_controller_test.rb
@@ -3,4 +3,27 @@
 require 'test_helper'
 
 class TimeControllerTest < ActionDispatch::IntegrationTest
+  test 'response is successful' do
+    get '/time/current'
+
+    assert_response :success
+  end
+
+  test "'Content-Type' header contains 'application/json'" do
+    get '/time/current'
+
+    assert_match %r{application\/json}, response.header['Content-Type']
+  end
+
+  test "'currentTime' value in JSON response is current time UTC" do
+    Timecop.freeze
+    get '/time/current'
+
+    current_time = Time.current
+    parsed_response = JSON.parse(response.body)
+
+    assert_equal current_time.strftime('%Y-%m-%d %H:%M:%S'), parsed_response['currentTime']
+
+    Timecop.return
+  end
 end
