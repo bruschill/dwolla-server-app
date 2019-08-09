50b4b411a2c90d8f8d8daab64d9ad6edb8ad8f91
B. Ruschill <brandon.ruschill@gmail.com>
2019-08-08 21:22:02 -0500

    add cors middleware config

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module ServerApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:4567'
+        resource '/time/current', methods: [:get]
+      end
+    end
   end
 end
